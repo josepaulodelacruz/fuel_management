61146dbbbbd254865b9aba09a05f317ae8a3bc06
Lester Jay <mercadolesterjay@gmail.com>
2025-03-13 22:50:14 +0800

    Update routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -2,15 +2,16 @@ import { type RouteConfig, index, layout, route } from "@react-router/dev/routes
 import StringRoutes from "./Constants/StringRoutes.js";
 
 export default [
-    //temporary initial screen
-    index("routes/home.tsx"),
+  index("./Pages/Dev/index.jsx"),
 
   /*
    * - Layout
    * add your page/views inside the layout
    */
-  //pattern
-  route(StringRoutes.dev, "Pages/Dev/index.jsx"),
-  route(StringRoutes.fuelManagement, "Pages/FuelManagement.jsx")
+  layout("./Layouts/DasboardLayout.jsx", [
+    //initial dashboard page
+    route(StringRoutes.dashboard, "./Pages/Dashboard/index.jsx"),
+    route(StringRoutes.fuelManagement, "Pages/FuelManagement.jsx")
+  ])
 
 ] satisfies RouteConfig;
