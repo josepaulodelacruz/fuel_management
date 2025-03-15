c63b72201ab105fd7806bc361bd129b7f021f735
josepaulodelacruz <whoseepowlu@gmail.com>
2025-03-15 11:35:09 +0800

    feat: toggle sidebar functionality

--- app/Components/Sidebar.jsx
+++ app/Components/Sidebar.jsx
@@ -11,14 +11,16 @@ import {
   MapPinIcon,
   GlobeIcon,
 } from 'lucide-react'
-
+import useToggleDrawer from '~/Hooks/Sidenav/useToggleDrawer'
 
 /*
  * Sidebar sample 
  * */
 const Sidebar = () => {
+  const { isOpen } = useToggleDrawer();
+
   return (
-    <div className="w-60 bg-blue-50 flex flex-col h-full ">
+    <div className="bg-blue-50 flex flex-col h-full">
       <div className="p-4">
         <div className="flex items-center">
           <img
@@ -26,7 +28,14 @@ const Sidebar = () => {
             alt="Fortunewell Logo"
             className="mr-2"
           />
-          <div>
+          <div
+            style={{
+              zIndex: isOpen ? '1' : '-1',
+              transform: isOpen ? 'translateX(0px)' : 'translateX(-10px)',
+              opacity: isOpen ? 1 : 0,
+              transition: 'transform 0.3s ease, opacity 0.3s ease',
+            }}
+          >
             <h1 className="text-lg font-bold text-blue-900">Fortunewell</h1>
             <p className="text-xs text-gray-600">
               Integrated Fuel Management System
@@ -34,33 +43,10 @@ const Sidebar = () => {
           </div>
         </div>
       </div>
-      <div className="p-4 ">
-        <div className="flex items-center">
-          <UserIcon className="mr-2 text-gray-600" size={24} />
-          <div>
-            <div className="flex items-center">
-              <span className="font-medium text-gray-700">Alice Feeney</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-4 w-4 ml-1"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M19 9l-7 7-7-7"
-                />
-              </svg>
-            </div>
-            <p className="text-xs text-gray-600">Admin</p>
-          </div>
-        </div>
-      </div>
+
       <nav className="flex-1 overflow-y-auto">
         <ul>
+          <AdminNavItem />
           <NavItem
             icon={<LayoutDashboardIcon size={18} />}
             text="Dashboard"
@@ -76,56 +62,6 @@ const Sidebar = () => {
             icon={<PackageIcon size={18} />}
             text="Inventory Management"
           />
-          <NavItem icon={<FileTextIcon size={18} />} text="Accounting" />
-          <li className="mt-4">
-            <div className="px-4 py-2">
-              <div className="flex items-center justify-between text-gray-600">
-                <span className="text-sm font-medium">Setup</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M19 9l-7 7-7-7"
-                  />
-                </svg>
-              </div>
-            </div>
-          </li>
-          <NavItem icon={<MapPinIcon size={18} />} text="Stations" indented />
-          <NavItem
-            icon={<GlobeIcon size={18} />}
-            text="Global Records"
-            indented
-          />
-          <li className="mt-4">
-            <div className="px-4 py-2">
-              <div className="flex items-center justify-between text-gray-600">
-                <span className="text-sm font-medium">Settings</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M19 9l-7 7-7-7"
-                  />
-                </svg>
-              </div>
-            </div>
-          </li>
-          <NavItem icon={<Users2Icon size={18} />} text="Users" indented />
         </ul>
       </nav>
     </div>
@@ -133,6 +69,8 @@ const Sidebar = () => {
 }
 
 const NavItem = ({ icon, text, active = false, indented = false }) => {
+  const { isOpen } = useToggleDrawer();
+
   return (
     <li>
       <a
@@ -140,9 +78,54 @@ const NavItem = ({ icon, text, active = false, indented = false }) => {
         className={`flex items-center px-4 py-2 ${active ? 'bg-blue-100 text-blue-800' : 'text-gray-600 hover:bg-blue-50'} ${indented ? 'pl-8' : ''}`}
       >
         <span className="mr-3">{icon}</span>
-        <span className="text-sm">{text}</span>
+        <span
+          style={{
+            transform: isOpen ? 'translateX(0px)' : 'translateX(-10px)',
+            opacity: isOpen ? 1 : 0,
+            transition: 'transform 0.3s ease, opacity 0.3s ease',
+          }}
+          className="text-sm">{text}</span>
       </a>
     </li>
+  )
+}
+
+const AdminNavItem = () => {
+  const { isOpen } = useToggleDrawer();
+
+  return (
+    <li className="flex flex-row items-center px-3">
+      <span className='mr-2'>
+        <UserIcon size={24} />
+      </span>
+      <div
+        style={{
+          transform: isOpen ? 'translateX(0px)' : 'translateX(-10px)',
+          opacity: isOpen ? 1 : 0,
+          transition: 'transform 0.3s ease, opacity 0.3s ease',
+        }}
+      >
+        <div className="flex items-center">
+          <span className="font-medium text-gray-700">Alice Feeney</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4 ml-1"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M19 9l-7 7-7-7"
+            />
+          </svg>
+        </div>
+        <p className="text-xs text-gray-600">Admin</p>
+      </div>
+    </li>
+
   )
 }
 
